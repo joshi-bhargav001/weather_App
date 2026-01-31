9cb0671a3a9f398661dbc87ee7434b8f21198ec4
Joshi Bhargav <joshibhargav008@gmail.com>
2026-01-31 14:50:57 +0530

    My project is completed

--- src/components/Myapp.jsx
+++ src/components/Myapp.jsx
@@ -5,12 +5,13 @@ import clear from '../images/clear.jpg'
 import mist from '../images/mist.png'
 import rain from '../images/rain.jpg'
 import smoke from '../images/smoke.png'
-// import error from '../images/error.jpg'
+import errors from '../images/error.jpg'
 
 function Myapp() {
 
       const [search, setSearch] = useState('')
       const [data, setData] = useState()
+      const [error, setError] = useState()
       const key = "ba55de13a7547027607bcc88069e83f3"
 
       const input_handle = (e) => {
@@ -25,16 +26,21 @@ function Myapp() {
         setData(jsondata)
 
         if(search == "") {
-          alert('Enter the city name');
+          setError('Enter the city name');
+        } else if(jsondata.cod == '404') {
+          setError('Please Enter the valid name');
+        } else {
+          setError('')
         }
+
         setSearch('')
       }
 
   return (
     <>
-    <div className="min-h-screen bg-black flex justify-center pt-10"> 
-      <div className='w-[320px] bg-white rounded-2xl shadow-xl p-6 mb-2'>
-            <h1 className="text-3xl font-bold text-blue-500 mb-2"> Weather App </h1>
+    <div className="min-h-screen bg-blue-300 flex justify-center pt-10"> 
+      <div className='w-[320px] h-[550px] bg-blue-200 rounded-2xl shadow-xl p-6 mb-2'>
+            <h1 className="text-3xl font-bold text-blue-500 mb-2 pb-1"> Weather App </h1>
 
         <div className='flex items-center bg-sky-100 rounded-full px-4 py-2'>
           <input className='bg-transparent outline-none flex-1 text-sm' value={search} type="text" onChange={input_handle} placeholder='Enter City, Country' />
@@ -45,10 +51,17 @@ function Myapp() {
 
         <div className=''>
           {
+            error ? 
+            <div className='mt-6'>
+              <p className='mt-5 mb-5 text-lg'>{error}</p>
+              <img className='' src={errors} alt="" />
+            </div> : ""
+          }
+          {
             data && data.weather ?
             <div className='my-4 '>
               <h2 className="text-center text-lg font-semibold mt-4">{data.name}</h2>
-              <div className="mt-3 mb-3">
+              <div className="my-3 mx-3">
                 <img src={data.weather[0].main == "Clouds" ? cloude : ""} />
                 <img src={data.weather[0].main == "Rain" ? rain : ""} />
                 <img src={data.weather[0].main == "Clear" ? clear : ""} />
